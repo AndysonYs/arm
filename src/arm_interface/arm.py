7e2efe0d04e55e6c52e93e52947877860e68c051
AndysonYs <yangshuai460@sina.com>
2019-06-06 18:31:37 +0800

    add arm_interface

---
+++ src/arm_interface/arm.py
@@ -0,0 +1,45 @@
+import arm_control.joint_trajectory_controller
+
+def init(jointMsg, jointHandler):
+    # check joint message
+    if jointHandler.check(jointMsg) == False):
+        return False
+
+    jointHander.init()
+    # check TF-Tree
+    if not jointHandler.TFtree.valid():
+        return False
+    return True
+
+def lowerArmMove(target, jointHandler):
+    init()
+    for location in target:
+        jointHandler.desire_state.enQueue(location)
+    jointHandler.plan_upper.setFalse()
+    jointHandler.plan_lower.setTrue()
+
+    for location in jointHandler.queue:
+    jointHandler.updateCommand(
+        jointHandler.calTime(),     # time
+        jointHandler.calDuration(), # period
+        jointHandler.cur_target,
+        jointHander.error_Handler
+    )
+    return jointHandler.starting()
+
+def upperArmMove(target, jointHandler):
+    init()
+    for location in target:
+        jointHandler.desire_state.enQueue(location)
+    jointHandler.plan_upper.setTrue()
+    jointHandler.plan_lower.setFalse()
+
+    for location in jointHandler.queue:
+    jointHandler.updateCommand(
+        jointHandler.calTime(),     # time
+        jointHandler.calDuration(), # period
+        jointHandler.cur_target,
+        jointHander.error_Handler
+    )
+    return jointHandler.starting()
+
