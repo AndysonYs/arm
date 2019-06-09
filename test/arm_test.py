fad44c12b8300ae80e6d279dce913a0e45d9be6e
AndysonYs <yangshuai460@sina.com>
2019-06-09 19:23:36 +0800

    revise test path

--- test/arm_test.py
+++ test/arm_test.py
@@ -2,7 +2,9 @@ from arm_interface import *
 import arm_control.joint_trajectory_controller as jointHandler
 from arm_description import jointMsg
 import time as tm
-
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))+'/src')
 
 def toLog(time, level, path, info):
     LEVEL = ["WARNING", "ERROR", "INFO"]
--- test/claw_test.py
+++ test/claw_test.py
@@ -1,7 +1,9 @@
 from arm_interface import *
 import arm_control.joint_trajectory_controller as jointHandler
 import time as tm
-
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))+'/src')
 
 def toLog(time, level, path, info):
     LEVEL = ["WARNING", "ERROR", "INFO"]
